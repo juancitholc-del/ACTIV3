ff090142f47700e78af02b247797c1933c4f5157
juancitholc-del <juancitho.lc@gmail.com>
2026-06-14 19:47:57 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+Console.WriteLine("=== Cálculo de Descuento para Clientes ===");
+Console.WriteLine("---------------------------------------------");
+
+string tipoCliente;
+double compra, descuento;
+
+Console.Write("Ingrese tipo de cliente (VIP o NORMAL): ");
+tipoCliente = Console.ReadLine();
+
+Console.Write("Ingrese monto de compra: ");
+compra = double.Parse(Console.ReadLine());
+
+if (tipoCliente.ToUpper() == "VIP")
+{
+    if (compra > 500)
+    {
+        descuento = compra * 0.20;
+    }
+    else
+    {
+        descuento = compra * 0.10;
+    }
+}
+else
+{
+    if (compra > 500)
+    {
+        descuento = compra * 0.05;
+    }
+    else
+    {
+        descuento = 0;
+    }
+}
+
+Console.WriteLine($"Descuento: S/{descuento}" );
+Console.WriteLine("Total a pagar: S/" + (compra - descuento));
+
+Console.ReadKey();
